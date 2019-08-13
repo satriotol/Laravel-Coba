d15d28436adacf2126d8369b25d49fb072533250
arifahdip <r1v4hw1to@gmail.com>
2019-08-13 15:10:23 +0700

    navbar

--- laravel/resources/views/includes/navbar.blade.php
+++ laravel/resources/views/includes/navbar.blade.php
@@ -1,3 +1,40 @@
+<link href="{{ asset('css/icono.min.css') }}" rel="stylesheet">
+<style>
+        .user-menu {
+            display: inline-block;
+            margin-top: 10px;
+            margin-right: 10px;
+            float: right;
+            list-style: none;
+            padding: 0;
+        }
+    </style>
 <section>
-    <h1>Ini Navbar</h1>
+    <nav class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar" style="background-color: #e3f2fd;" role="navigation">
+        <div class="container-fluid">
+
+            <div class="navbar-header"> 
+                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
+                    data-target="#sidebar-collapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">POS - Multi Outlets</a>
+                <ul class="navbar-nav flex-row d-lg-inline-block my-2 my-sm-0">
+                <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+				</ul>
+
+
+            </div>
+        </div>
+        
+    </nav>
 </section>
