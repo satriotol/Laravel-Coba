3aed615d96ae0086f28c7e08b4aa14eb35b7434c
anggabaeee <bayupratam8666@gmail.com>
2019-08-13 13:01:18 +0700

    logine pak

--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -15,7 +15,7 @@
         <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
             <div class="login-panel panel panel-default">
                 <div class="panel-heading" style="height: 175px;">
-                    Account Access
+                    Jahat
                     <br>
                     <img src="{{ asset('img/logo.jpg') }}" height="100px">
                 </div>
